0794649760053a210c1db08fd1dfc75d596a31ea
Joicy_Reis <joicy.reis@youxgroup.com.br>
2021-10-14 20:38:38 -0300

    iniciando projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get("//", function(req, res) {
+    res.send("oi teste");
+});
+
+let porta = process.emit.PORT || 4000;
+
+app.listen(3000, function() {
+    console.log("Servidor rodando na porta " + porta);
+})
